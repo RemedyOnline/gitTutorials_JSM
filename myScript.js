a2a7ad77b744883871423f05296510c7db38695a
Mike <agyemangmichael555@gmail.com>
2025-03-12 18:16:04 +0000

    added wednesday updates + ghana image

--- myScript.js
+++ myScript.js
@@ -1,2 +1,4 @@
 console.log('JS Mastery is the best bro!!');
 console.log('Their detailed explanations are the best and down to earth..');
+
+console.log("Today is Thursday - 12th March, 2025")
--- test.js
+++ test.js
@@ -1,1 +1,4 @@
 console.log('Testing Git Tutorials!');
+
+console.log("I'm so glad, I was able to make it to the GTP merhhnnn!")
+console.log("Big shoutouts to you, Onyankop)n!!!!!!!!!!!!!!!!!!!!!!!")
