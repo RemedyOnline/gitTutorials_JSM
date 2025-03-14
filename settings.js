02f5aae6005efb4e4ed124b7efe32a4947389406
Mike <agyemangmichael555@gmail.com>
2025-03-14 12:55:37 +0000

    successfully figured out how GIT STASH works, and I was also able to resolve the conflict involved in this operation as well... #Kudos to me bro!!!!!!!!!!!!!

--- settings.js
+++ settings.js
@@ -2,3 +2,8 @@ console.log("DAY: Friday")
 console.log("DATE: March 14, 2025")
 
 console.log("User Settings Page Created...")
+
+
+console.log("So let's say I just started working on the Settings page, just 14% through..")
+console.log("then my supervisor alerts me to go and fix and error or to go and insert a line above the first line on this page")
+console.log("Probably, write the day and date on the page for him to see as and when I do anything...")
